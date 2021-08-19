920e3ca5d1fef8e5570f58714577f83ecc0ef551
multikyd <72329880+multikyd@users.noreply.github.com>
2021-08-19 17:09:34 +0900

    openpilot v0.8.8 release

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -277,6 +277,8 @@ class CarInterface(CarInterfaceBase):
 
     ret = self.CS.update(self.cp, self.cp2, self.cp_cam)
     ret.canValid = self.cp.can_valid and self.cp2.can_valid and self.cp_cam.can_valid
+    if not self.cp.can_valid or not self.cp2.can_valid or not self.cp_cam.can_valid:
+      print('cp={}  cp2={}  cp_cam={}'.format(bool(self.cp.can_valid), bool(self.cp2.can_valid), bool(self.cp_cam.can_valid)))
     ret.steeringRateLimited = self.CC.steer_rate_limited if self.CC is not None else False
 
     if self.CP.pcmCruise and not self.CC.scc_live:
